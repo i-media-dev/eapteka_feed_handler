2f1811ed73c44216775008546236ef5e36a26edf
i-media-dev <mrk_dev@i-media.ru>
2025-10-13 18:38:23 +0300

    added call replace images method in main

--- handler/main.py
+++ handler/main.py
@@ -15,6 +15,7 @@ def main():
     # handler_client.inner_join_feeds()
     image_client.get_images()
     image_client.add_frame()
+    handler_client.image_replacement()
 
 
 if __name__ == '__main__':
