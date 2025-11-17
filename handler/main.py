0a2c960f52f2750512669d79aad8d1232375abce
i-media-dev <mrk_dev@i-media.ru>
2025-11-17 15:11:47 +0300

    workfully

--- handler/main.py
+++ handler/main.py
@@ -1,21 +1,21 @@
 # from handler.constants import CUSTOM_LABEL, UNAVAILABLE_OFFER_ID_LIST
 from handler.decorators import time_of_function, time_of_script
-from handler.utils import initialize_components  # save_to_database
+from handler.utils import initialize_components, save_to_database
 
 
 @time_of_script
 @time_of_function
 def main():
     saver, handler_client, db_client, image_client = initialize_components()
-    # saver.save_xml()
-    # data = handler_client.get_offers_report()
-    # save_to_database(db_client, data)
-    # # handler_client.process_feeds(CUSTOM_LABEL, UNAVAILABLE_OFFER_ID_LIST)
-    # handler_client.full_outer_join_feeds()
-    # handler_client.inner_join_feeds()
-    # image_client.get_images()
-    # image_client.add_frame()
-    # handler_client.image_replacement()
+    saver.save_xml()
+    data = handler_client.get_offers_report()
+    save_to_database(db_client, data)
+    # handler_client.process_feeds(CUSTOM_LABEL, UNAVAILABLE_OFFER_ID_LIST)
+    handler_client.full_outer_join_feeds()
+    handler_client.inner_join_feeds()
+    image_client.get_images()
+    image_client.add_frame()
+    handler_client.image_replacement()
     handler_client.image_replacement(
         ['full_outer_join_feed.xml', 'inner_join_feed.xml']
     )
